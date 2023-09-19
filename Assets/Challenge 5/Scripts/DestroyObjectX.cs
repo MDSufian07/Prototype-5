751c0c30fffc4526795aa2839d79315eb40db314
MDSufian07 <113298778+MDSufian07@users.noreply.github.com>
2023-09-20 02:06:46 +0600

    after challenge

---
+++ Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject,5.0f); // destroy particle after 5 seconds
+    }
+}
